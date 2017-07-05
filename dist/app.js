37bf909aca4f87dbeda0e13481d7eb06e5a0f4fe
Vtek <pontoreau.sylvain@gmail.com>
2017-07-05 20:13:53 +0200

    Application root file

---
+++ dist/app.js
@@ -0,0 +1,6 @@
+"use strict";
+window.onload = function () {
+    var container = document.getElementById('container');
+    if (container != null)
+        container.innerHTML = 'test';
+};
---
+++ src/app.ts
@@ -0,0 +1,5 @@
+window.onload = () => {
+    const container = document.getElementById('container');
+    if(container != null)
+        container.innerHTML = 'test';
+};
