8a8a71add1042201ebb75c3bc6a3b3613bb18a76
Vtek <pontoreau.sylvain@gmail.com>
2017-07-07 19:39:06 +0200

    Basic container features implemented

--- src/app/infrastructure/container.ts
+++ src/app/infrastructure/container.ts
@@ -1,7 +1,24 @@
 import { Module } from "./module";
+import { Dependency } from "./dependency";
 
 export class Container {
+    private dependencies: Array<Dependency> = [];
     constructor(...modules: Module[]) {
         modules.forEach((module) => module.load(this));
+    }
+
+    register(dependency: Dependency) {
+        this.dependencies.push(dependency);
+    }
+
+    getInstance<T>(className: string): T {
+        this.dependencies.forEach((dependency) => {
+            if(className == dependency.className) {
+                const c = (dependency.ctor as new() => T);
+                return (new c() as T);
+            }
+        });
+
+        throw new Error(`Unknow type ${className}`);
     }
 }
