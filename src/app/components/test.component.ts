dfad6c77093888cb596a3e0f288891a764632980
Vtek <pontoreau.sylvain@gmail.com>
2017-07-09 18:41:29 +0200

    Rewrite template return of TestComponent

--- src/app/components/test.component.ts
+++ src/app/components/test.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '../infrastructure/component';
+import { VirtualDOM } from "../infrastructure/dom";
 
 export class TestComponent extends Component {
     tag(): string {
         return 'test';
     }
-    template(): string {
-        return '<h1>This component is a little test to start the project !</h1>';
+    template(): VirtualDOM {
+        return {
+            tag: 'test',
+            childs: [{
+                    tag: 'h1',
+                    childs: [
+                        'This component is a little test to start the project !'
+                    ]
+                }, {
+                    tag: 'span',
+                    childs: [
+                        'This is awesome to render a Virtual DOM component !!', {
+                            tag: 'ul',
+                            childs: [{ 
+                                    tag: 'li',
+                                    childs: ['test1']
+                                }, { 
+                                    tag: 'li',
+                                    childs: ['test2']
+                                }, { 
+                                    tag: 'li',
+                                    childs: ['test2']
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        };
     }
 }
